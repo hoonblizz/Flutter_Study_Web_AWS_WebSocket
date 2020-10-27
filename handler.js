25a30a7b3fc3f87c7240f4f75eb783528746f85a
Taehoon <hoonblizz@gmail.com>
2020-10-27 00:55:52 -0400

    simplified

--- handler.js
+++ handler.js
@@ -1,10 +1,6 @@
 'use strict';
 const AWS = require("aws-sdk");
-const { v4: uuidv4 } = require('uuid');
 require("aws-sdk/clients/apigatewaymanagementapi");
-const dynamodb = new AWS.DynamoDB();
-const docClient = new AWS.DynamoDB.DocumentClient();
-const tableName = "flutterSlsTable";
 
 const success = {
   statusCode: 200
@@ -40,14 +36,15 @@ module.exports.defaultHandler = async (event, context) => {
 
 module.exports.databaseStreamHandler = async (event, context) => {
   console.log(JSON.stringify(event, null, 2));
-
-  // Testing with local command using like, 
-  // sls invoke -f databaseStreamHandler -d '{"randNum": 12345, "msg": "msg from invoke", "connectionId": "U9JwfeC1IAMCFIQ=", "domainName": "us2q8s4g99.execute-api.us-east-1.amazonaws.com", "stage": "dev"}'
-  const randNum = event.randNum;
-  const msg = event.msg;
+  /*
+    Testing from application using web socket
+  */
+  const body = JSON.parse(event.body);
+  const randNum = body.randNum;
+  const msg = body.msg;
   
-  let connectionId = event.connectionId;
-  const endpoint = event.domainName + "/" + event.stage;
+  let connectionId = event.requestContext.connectionId;
+  const endpoint = event.requestContext.domainName + "/" + event.requestContext.stage;
   const apigwManagementApi = new AWS.ApiGatewayManagementApi({
       apiVersion: "2018-11-29",
       endpoint: endpoint
@@ -58,22 +55,5 @@ module.exports.databaseStreamHandler = async (event, context) => {
     };
   return apigwManagementApi.postToConnection(params).promise();
 
-  // Testing from application using web socket
-  
-  // const body = JSON.parse(event.body);
-  // const randNum = body.data.body.randNum;
-  // const msg = body.data.body.msg;
-  
-  // let connectionId = event.requestContext.connectionId;
-  // const endpoint = event.requestContext.domainName + "/" + event.requestContext.stage;
-  // const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-  //     apiVersion: "2018-11-29",
-  //     endpoint: endpoint
-  //   });
-  // const params = {
-  //     ConnectionId: connectionId,
-  //     Data: JSON.stringify({connectionID: connectionId, randNum: randNum, msg: msg}),
-  //   };
-  // return apigwManagementApi.postToConnection(params).promise();
 };
 
