66bf10e1b34c794c1a1472e62a19c72c7295021f
Taehoon <hoonblizz@gmail.com>
2020-11-05 14:22:52 -0500

    modified finished CONNECTED collection

--- handler.js
+++ handler.js
@@ -176,9 +176,13 @@ module.exports.createConnectedUser = async (event, context) => {
   return JSON.stringify(response);
 }
 
-/// Get all connected users
+/// Get ALL connected users
 module.exports.getConnected = async (event, context, callback) => {
+
   let response = {};
+
+  console.log('Received parameter in event: ', JSON.stringify(event, null, 2));
+
   // With in "CONNECTED" collection, 
   // check if key "userName" exists and it's not null
   // 식 속성 이름 부르는 방법: Nested 같은 경우엔 하나씩 ExpressionAttributeNames 에 불러준 다음 Dot (.) 으로 써준다.
@@ -215,12 +219,63 @@ module.exports.getConnected = async (event, context, callback) => {
     response = {
       statusCode: 400,
       message: "Scan error",
-      body: JSON.stringify(err, null, 2),
+      body: err,
     };
     console.error("Unable to get item. Error JSON:", JSON.stringify(err, null, 2));
   }
 
-  callback(null, response);
+  //callback(null, response);
+  return JSON.stringify(response);
+};
+
+/// Get a single user (check if exists)
+module.exports.getConnectedUser = async (event, context, callback) => {
+
+  let response = {};
+
+  console.log('Received parameter in event: ', JSON.stringify(event, null, 2));
+  let {userName} = event;
+
+  var params = {
+    TableName: process.env.TABLE_NAME,
+    FilterExpression: "#collectionName = :cname AND (attribute_exists(#data.#userName) AND #data.#userName = :userName)",
+    ExpressionAttributeNames:{
+      "#collectionName": "collection",
+      "#data": "data",
+      "#userName": "userName"
+    },
+    ExpressionAttributeValues: {
+      ":cname": "CONNECTED",
+      ":userName": userName
+    }
+  }
+
+  try {
+    const data = await docClient.scan(params).promise();
+    // data.Items 라는 Array 로 나온다.
+    // Scan 은 최대 1MB 까지만 가져오므로, 1MB 가 넘을때를 대비한 코드도 필요하지만, 여기서는 넘어가자. 
+    // 여기서 더 찾아볼수 있다.
+    // https://docs.aws.amazon.com/ko_kr/amazondynamodb/latest/developerguide/GettingStarted.NodeJs.04.html
+    response = {
+      statusCode: 200,
+      message: "Scan completed",
+      body: data.Items,
+    };
+    console.log('Printing all scanned items....');
+    data.Items.forEach((el) => {
+      JSON.stringify(el, null, 2)
+    });
+  } catch(err) {
+    response = {
+      statusCode: 400,
+      message: "Scan error",
+      body: err,
+    };
+    console.error("Unable to get item. Error JSON:", JSON.stringify(err, null, 2));
+  }
+
+  //callback(null, response);
+  return JSON.stringify(response);
 };
 
 /// Add Connected user
@@ -273,15 +328,48 @@ module.exports.updateConnected = async (event, context, callback) => {
     response = {
       statusCode: 400,
       message: "Update error",
-      body: JSON.stringify(err, null, 2),
+      body: err,
     };
     console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
   }
 
-  callback(null, response);
+  //callback(null, response);
+  return JSON.stringify(response);
 };
 
-module.exports.deleteConnected = async (event, context, callback) => {};
+module.exports.deleteConnected = async (event, context, callback) => {
+  let response = {};
+
+  console.log('Received parameter in event: ', JSON.stringify(event, null, 2));
+  let {userName} = event;
+
+  var params = {
+    TableName:table,
+    Key:{
+      "collection": "CONNECTED",
+      "subCollection": "CONNECTED#USER#" + userName,
+    }
+  };
+
+  try {
+    const data = await docClient.delete(params).promise();
+    response = {
+      statusCode: 200,
+      message: "Delete completed",
+      body: data,
+    };
+    console.log("Delete succeeded:", JSON.stringify(data, null, 2));
+  } catch(err) {
+    response = {
+      statusCode: 400,
+      message: "Delete error",
+      body: err,
+    };
+    console.error("Unable to Delete item. Error JSON:", JSON.stringify(err, null, 2));
+  }
+
+  return response;
+};
 
 /******************************************************************
  * MSG Colleciton handlers
